bc0607d4e82c5de9931186437c656b09c4bc3a30
Krai53n <greasha46@gmail.com>
2021-03-12 17:53:21 +0300

    (⊙_⊙)

--- hamagui.py
+++ hamagui.py
@@ -1,14 +1,13 @@
 import requests
 import subprocess
+
 from os import remove
+from os import uname
+
 from tkinter import Tk
 
 
-# NOTE:
-# Commands class can be done easyier. So goal make it more understable
-
-
-URL_SITE = "https://www.vpn.net/installers/"
+URL_SITE_DL = "https://www.vpn.net/installers/"
 OPTIONS = {
     "linux": {
         32: {
@@ -21,9 +20,23 @@ OPTIONS = {
 }
 
 
+def get_os_information():
+    """
+    Return os name and type of architecture
+    """
+    inf = list(uname())
+    if inf[4] == "x86_64":
+        inf[4] = 64
+    if inf[4] == "64":
+        inf[4] = 32
+    return inf[0].lower(), inf[4]
+
+def execute_command(command, path=None):
+    pass
+
 class Install:
     def download(os, bit):
-        url = URL_SITE + OPTIONS[os][bit]["tgz"]
+        url = URL_SITE_DL + OPTIONS[os][bit]["tgz"]
         r = requests.get(url, allow_redirects=True)
         open(OPTIONS[os][bit]["tgz"], "wb").write(r.content)
 
@@ -37,31 +50,16 @@ class Install:
         remove(OPTIONS[os][bit]["tgz"])
 
 
-class Commands:
-    def execute_install_sh(path):
-        """
-        Install install.sh file
-        """
-        name = "install.sh"
-        subprocess.run(["sh", "{}".format(path+name)])
-
-    def init_hamachi(path):
-        name = "hamachid"
-        subprocess.run([".{}".format(path+name)])
-
-    def kill_hamachi_process():
-        subprocess.run(["killall", "hamachid"])
-
-
 class Gui:
     def __init__(self):
         pass
 
 
 if __name__ == "__main__":
-    Install.install("linux", 64)
+    print(get_os_information())
+    #Install.install("linux", 64)
     #path = "/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/"
-    path = "/logmein-hamachi-2.1.0.203-x64/"
-    Commands.execute_install_sh("/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/")
-    Commands.init_hamachi(path)
+    #path = "/logmein-hamachi-2.1.0.203-x64/"
+    #Commands.execute_install_sh("/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/")
+    #Commands.init_hamachi(path)
     #Commands.kill_hamachi_process()
