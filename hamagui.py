a5ea640680e839955fe834e1a9b03f43b035c491
Krai53n <greasha46@gmail.com>
2021-03-26 22:14:05 +0300

    ┐(￣∀￣)┌

--- hamagui.py
+++ hamagui.py
@@ -3,6 +3,11 @@ import subprocess
 
 from os import remove
 from os import uname
+from os import walk
+from os import getcwd
+from os import rename
+from os import rmdir
+import os.path
 
 from tkinter import Tk
 
@@ -40,14 +45,27 @@ class Install:
         r = requests.get(url, allow_redirects=True)
         open(OPTIONS[os][bit]["tgz"], "wb").write(r.content)
 
+    def remove_files(from_dir, to_dir):
+        """
+        Recursivly remove all files
+        """
+        for root, dirs, files in walk(from_dir):
+            for file in files:
+                rename(os.path.join(root, file), os.path.join(to_dir, file))
+
     def extract(os, bit):
-        subprocess.run(["tar", "--extract", "-f", OPTIONS[os][bit]["tgz"]])
+        from sys import exit
+        dir_name = OPTIONS[os][bit]["tgz"]
+        dir_path = getcwd() + "/" + dir_name[:-4]
+        subprocess.run(["tar", "--extract", "-f", dir_name])
+        Install.remove_files(dir_path, getcwd())
 
 
     def install(os, bit):
         Install.download(os, bit)
         Install.extract(os, bit)
         remove(OPTIONS[os][bit]["tgz"])
+        rmdir(OPTIONS[os][bit]["tgz"][:-4])
 
 
 class Gui:
@@ -56,8 +74,8 @@ class Gui:
 
 
 if __name__ == "__main__":
-    print(get_os_information())
-    #Install.install("linux", 64)
+    #print(get_os_information())
+    Install.install("linux", 64)
     #path = "/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/"
     #path = "/logmein-hamachi-2.1.0.203-x64/"
     #Commands.execute_install_sh("/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/")
--- todo.md
+++ todo.md
@@ -1,11 +1,9 @@
 ## TODO:
-- [ ] get type of os
-- [ ] type of arhitecre
--------------------------
 - [ ] undrestand how user can interact with cli of hamachi
 - [ ] make scretch of ui/ux
-- [ ] choose toolkit for creating of gui
 - [ ] make exception if user run script on the Windows
-- [ ] install.sh installed in not right way
 
 ## COMPLETE:
+- [x] get type of os
+- [x] type of arhitecre
+- [x] choose toolkit for creating of gui --> Qt
