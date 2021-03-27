b400b674d55887af48abf72a5d84125779d5c274
Krai53n <greasha46@gmail.com>
2021-03-27 18:57:39 +0300

    (<-_-><-_->)

--- hamagui/core.py
+++ hamagui/core.py
@@ -7,6 +7,7 @@ from os import walk
 from os import getcwd
 from os import rename
 from os import rmdir
+from os import system
 import os.path
 
 
@@ -34,8 +35,6 @@ def get_os_information():
         inf[4] = 32
     return inf[0].lower(), inf[4]
 
-def execute_command(command, path=None):
-    pass
 
 class Install:
     def download(os, bit):
@@ -43,7 +42,7 @@ class Install:
         r = requests.get(url, allow_redirects=True)
         open(OPTIONS[os][bit]["tgz"], "wb").write(r.content)
 
-    def remove_files(from_dir, to_dir):
+    def move_files(from_dir, to_dir):
         """
         Recursivly remove all files
         """
@@ -52,22 +51,57 @@ class Install:
                 rename(os.path.join(root, file), os.path.join(to_dir, file))
 
     def extract(os, bit):
-        from sys import exit
         dir_name = OPTIONS[os][bit]["tgz"]
         dir_path = getcwd() + "/" + dir_name[:-4]
         subprocess.run(["tar", "--extract", "-f", dir_name])
-        Install.remove_files(dir_path, getcwd())
+        Install.move_files(dir_path, getcwd())
 
     def install(os, bit):
         Install.download(os, bit)
         Install.extract(os, bit)
         remove(OPTIONS[os][bit]["tgz"])
         rmdir(OPTIONS[os][bit]["tgz"][:-4])
+
+
+class Mana:
+    """
+    Manipultaion with hamachi or just `Mana`
+    """
+    def __init__(self):
+        # hamachid - it`s main file of hamachi
+        # Be careful with it beacause of unknowing of considering in this file
+        self.hamachid = "hamachid"
+
+    def run_insall_sh(self):
+        """
+        Run install.sh script
+        """
+        system("./insatll.sh")
+        return 1
+
+    def power_on_hamachid(self):
+        """
+        Power on hamachid
+        hamachid - it`s main file of hamachi
+        Be careful with it beacause of unknowing of considering in this file
+        """
+        system("./{}".format(self.hamachid))
+        return 1
+    
+    def power_off_hamachid(self):
+        """
+        Power off hamachid
+        """
+        system("killall", self.hamachid)
+        return 1
         
 
 if __name__ == "__main__":
     #print(get_os_information())
-    Install.install("linux", 64)
+    mana = Mana()
+    #Install.install("linux", 64)
+    mana.run_insall_sh()
+    mana.power_on_hamachid()
     #path = "/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/"
     #path = "/logmein-hamachi-2.1.0.203-x64/"
     #Commands.execute_install_sh("/home/kra53n/Рабочий стол/hamagui/logmein-hamachi-2.1.0.203-x64/")
