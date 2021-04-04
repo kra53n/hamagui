03a8812c402a174ec5cc59763eaf471293d3b7bf
Krai53n <greasha46@gmail.com>
2021-04-04 12:46:52 +0300

    (￣▽￣)ノ

--- hamagui/gui.py
+++ hamagui/gui.py
@@ -1,3 +1,4 @@
+import getgit
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -23,6 +24,7 @@ class MainWindow(Gtk.Window):
         label = Gtk.Label(label="Status", expand=1)
         self.label_status = Gtk.Label(expand=1)
         self.button_status = Gtk.Button(expand=1)
+        self.button_status.connect("clicked", self.button_status_clicked)
         grid.add(label)
         grid.attach(self.label_status, 1, 0, 1, 1)
         grid.attach(self.button_status, 2, 0, 1, 1)
@@ -30,6 +32,7 @@ class MainWindow(Gtk.Window):
         label = Gtk.Label(label="Nickname", expand=1)
         self.entry_nickname = Gtk.Entry(placeholder_text="Krai53n", expand=1)
         self.button_nickname = Gtk.Button(label="Apply", expand=1)
+        self.button_nickname.connect("clicked", self.button_nickname_clicked)
         grid.attach(label, 0, 1, 1, 1)
         grid.attach(self.entry_nickname, 1, 1, 1, 1)
         grid.attach(self.button_nickname, 2, 1, 1, 1)
@@ -37,6 +40,7 @@ class MainWindow(Gtk.Window):
         label = Gtk.Label(label="Client ID", expand=1)
         self.label_client_id = Gtk.Label(label="234.234.234", expand=1)
         self.button_client_id = Gtk.Button(label="Copy", expand=1)
+        self.button_client_id.connect("clicked", self.button_client_id_clicked)
         grid.attach(label, 0, 2, 1, 1)
         grid.attach(self.label_client_id, 1, 2, 1, 1)
         grid.attach(self.button_client_id, 2, 2, 1, 1)
@@ -75,6 +79,25 @@ class MainWindow(Gtk.Window):
         self.label_client_id.props.label = mana.hamachi_inf()["client id"]
         self.label_address.props.label = mana.hamachi_inf()["address"]
         self.entry_join.props.placeholder_text = mana.hamachi_inf()["list"][-1]
+
+    def button_status_clicked(self, button):
+        label = self.label_status.get_label()
+        if label == "online":
+            mana.logged_off()
+        if label == "offline":
+            mana.logged_in()
+        self.update()
+
+    def button_nickname_clicked(self, button):
+        text = self.entry_nickname.get_text()
+        if text:
+            mana.set_nickname(self.entry_nickname.get_text())
+        self.update()
+
+    def button_client_id_clicked(self, button):
+        # TODO: use here Gtk.Clipboard
+        # https://python-gtk-3-tutorial.readthedocs.io/en/latest/clipboard.html
+        self.update()
 
 
 def main():
